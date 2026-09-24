e3c141947f05f1e9b466f9818504cd6a732f059a
ADITYA PATEL <175952359+AdityaPatel0921@users.noreply.github.com>
2026-09-24 22:31:44 +0530

    Time: 2 ms (11.58%), Space: 43.5 MB (7.67%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String toLowerCase(String s) {
+         String result = "";
+     for (int  i =  0; i < s.length(); i++){
+        // char ch = s.charAt(i);
+         result  =  s.toLowerCase();
+        
+
+     }
+     return result;
+        
+    }
+}
